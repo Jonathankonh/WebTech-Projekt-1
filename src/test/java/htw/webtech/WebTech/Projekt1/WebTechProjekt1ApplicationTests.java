f6b6f74c839ecc23919f336ffb2af56b0db7bc71
Jonathan <jonathankonstantin@icloud.com>
2026-04-10 08:47:00 +0200

    Erster Commit

---
+++ src/test/java/htw/webtech/WebTech/Projekt1/WebTechProjekt1ApplicationTests.java
@@ -0,0 +1,13 @@
+package htw.webtech.WebTech.Projekt1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTechProjekt1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
