8964b322cc0799a1184fa846d9cd0194fccf0bd1
Jonathan <jonathankonstantin@icloud.com>
2026-05-24 20:05:32 +0200

    M3 Frontend WebConfing hinzugefügt

---
+++ src/main/java/htw/webtech/WebTech/Projekt1/controller/WebConfig.java
@@ -0,0 +1,21 @@
+package htw.webtech.WebTech.Projekt1.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://investmind-frontend.onrender.com"
+                );
+    }
+}
